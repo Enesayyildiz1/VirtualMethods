d2d83f95ed93fe97e0ad1d1aa18655e3fb379c59
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2020-12-23 23:18:34 +0300

    Add project files.

---
+++ VirtualMethodss/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    public class Database
+    {
+        public virtual void  Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+    class SqlDatabase:Database 
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql server");
+        }
+    }
+    class MySql:Database 
+    {
+
+    }
+}
---
+++ VirtualMethodss/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+            database.Add();
+            
+            SqlDatabase sqlDatabase = new SqlDatabase();
+            sqlDatabase.Add();
+            Console.Read();
+        }
+    }
+}
